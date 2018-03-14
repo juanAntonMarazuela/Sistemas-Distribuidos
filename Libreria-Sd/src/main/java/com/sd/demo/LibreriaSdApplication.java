04070b1d456da033b20212764e1f5214be17616f
juanAntonMarazuela <je.anton@alumnos.urjc.es>
2018-03-14 13:27:16 +0100

    Init project

---
+++ Libreria-Sd/src/main/java/com/sd/demo/LibreriaSdApplication.java
@@ -0,0 +1,12 @@
+package com.sd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaSdApplication.class, args);
+	}
+}
