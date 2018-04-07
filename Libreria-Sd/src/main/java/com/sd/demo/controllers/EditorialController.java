cbd2cdfeb0bc761a5c41f290d1075794616bea70
analpa1997 <a.alcojor@alumnos.urjc.es>
2018-04-07 20:21:15 +0200

    Comenzando a crear editar Editorial

--- Libreria-Sd/src/main/java/com/sd/demo/controllers/EditorialController.java
+++ Libreria-Sd/src/main/java/com/sd/demo/controllers/EditorialController.java
@@ -1,13 +1,19 @@
 package com.sd.demo.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sd.demo.entity.Editorial;
+import com.sd.demo.entity.Libro;
 import com.sd.demo.reposository.EditorialRepository;
 
 @Controller
@@ -28,6 +34,45 @@ public class EditorialController {
 	@RequestMapping("/nuevaEditorial")
 	public String nuevaEditorial() {
 		return "nuevaEditorial";
+	}
+	
+	@RequestMapping("/editorial/editar/{id}")
+	public String editarLibro (Editorial editorial, Model model, @PathVariable Long id, @RequestParam(name="send",required=false ) String enviar) {
+		
+		
+		Optional<Editorial> editorialAntiguaOp = editRep.findById(id.longValue());
+		Editorial editorialAntigua = editorialAntiguaOp.get();
+		
+		boolean isEdition;
+		
+		isEdition = (enviar!=null);
+		
+		/* Se estan enviando los datos desde el formulario*/
+		if (isEdition) {
+			
+			if (!editorial.getNombre().isEmpty())
+				editorialAntigua.setNombre(editorial.getNombre());
+			
+			if (!editorial.getEmail().isEmpty())
+				editorialAntigua.setEmail(editorial.getEmail());
+			
+			if (!editorial.getDireccionPostal().isEmpty())
+				editorialAntigua.setDireccionPostal(editorial.getDireccionPostal());
+			if (!editorial.getCodigoIF().isEmpty())
+				editorialAntigua.setCodigoIF(editorial.getCodigoIF());
+			
+			editorialAntigua.setNumTelefono(editorial.getNumTelefono());
+			
+			editorialAntigua = editRep.save(editorialAntigua);
+					
+		}
+
+		List<Editorial> editoriales = editRep.findAll();
+		model.addAttribute("editoriales", editoriales);
+		model.addAttribute("editorial", editorialAntigua);
+		model.addAttribute("hasBeenUpdated", isEdition);
+		
+		return "editarEditorial";
 	}
 
 }
